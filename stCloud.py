1c56c64ee81d25ad7b9202b63cb6a9b690ab23fa
netlops <telorzpiidd@gmail.com>
2021-12-21 23:47:30 +0800

    添加一个404页面

--- stCloud.py
+++ stCloud.py
@@ -33,20 +33,22 @@ def searchContent(data={}, choice=None,content=None):
             answers.append(v)
     return questions,answers
 
-st.set_page_config(page_title='近代史答案Search', menu_items={
-    'Report a bug': "http://10.102.4.220:8033",
-    'About': "# 测试项目,近代史答案"
-})
-
-tiku = readJson()
-option = st.selectbox(
-    '请选择题模式',
-    ('单选题', '多选题'))
-content = st.text_input('问题', '运动')
-st.success("查询成功！")
-questions,answers = searchContent(data=tiku,choice=option, content=content)
-st.table(pd.DataFrame({
-    '问题': questions,
-    '答案': answers
-}))
+def jdsMain():
+    tiku = readJson()
+    option = st.selectbox(
+        '请选择题模式',
+        ('单选题', '多选题'))
+    content = st.text_input('问题', '运动')
+    st.success("查询成功！")
+    questions,answers = searchContent(data=tiku,choice=option, content=content)
+    st.table(pd.DataFrame({
+        '问题': questions,
+        '答案': answers
+    }))
+if __name__ == "__main__":
+    st.set_page_config(page_title='近代史答案Search', menu_items={
+        'Report a bug': "http://10.102.4.220:8033",
+        'About': "# 测试项目,近代史答案"
+    })
+    jdsMain()
 
